367c007002e358a1ae60ae7c29a9eea777a13fee
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2023-07-23 00:45:55 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -15,7 +15,7 @@ searchBtn.addEventListener("click", () => {
       //   console.log(Object.keys(data[0].currencies)[0]);
       //   console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
       //   console.log(
-      //     Object.values(data[0].languages).toString().split(",").join(", ")
+      //     Object.values(data[0].languages).toString().split(",").join(", ") ;
       //   );
       result.innerHTML = `
         <img src="${data[0].flags.svg}" class="flag-img">
